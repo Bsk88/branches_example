e7654efe87f385caac23cd72836b468c9d8f2162
sorokin_boris <BSorokin@lanit.ru>
2018-04-16 17:34:09 +0400

    Перенес файлы в новую директорию

--- branch/src/main/java/firstAutoTesterPackage/FirstAutoTester.java
+++ branch/src/main/java/firstAutoTesterPackage/FirstAutoTester.java
@@ -1,8 +1,10 @@
+package firstAutoTesterPackage;
+
 /**
  * Created by Sorokin Boris on 16.04.2018.
  */
 public class FirstAutoTester {
     public static void main(String[] args) {
-        System.out.println("The class created FirstAutoTester");
+        System.out.println("The class created firstAutoTesterPackage.FirstAutoTester");
     }
 }
