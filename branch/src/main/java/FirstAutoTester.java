2cf14403dfbc2c0ed2a4d0f73d752995c5a7dd5d
sorokin_boris <BSorokin@lanit.ru>
2018-04-16 18:10:31 +0400

    Второй автоматизатор изменил метод первого

--- branch/src/main/java/FirstAutoTester.java
+++ branch/src/main/java/FirstAutoTester.java
@@ -4,5 +4,6 @@
 public class FirstAutoTester {
     public static void main(String[] args) {
         System.out.println("The class created FirstAutoTester");
+        System.out.println("изменил метод 1 автотестера");
     }
 }
