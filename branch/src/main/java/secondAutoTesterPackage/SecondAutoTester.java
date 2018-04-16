d277ca9e502d86ca3e9921febc41de53612f2f92
sorokin_boris <BSorokin@lanit.ru>
2018-04-16 18:12:45 +0400

    Второй автоматизатор создал свой пакет и метод

---
+++ branch/src/main/java/secondAutoTesterPackage/SecondAutoTester.java
@@ -0,0 +1,10 @@
+package secondAutoTesterPackage;
+
+/**
+ * Created by Sorokin Boris on 16.04.2018.
+ */
+public class SecondAutoTester {
+    public static void main(String[] args) {
+        System.out.println("Второй автоматизатор создал свой класс");
+    }
+}
