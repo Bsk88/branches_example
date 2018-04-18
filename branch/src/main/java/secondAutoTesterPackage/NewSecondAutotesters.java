cf9b2b4480fe123e245a268451305f00431282f2
sorokin_boris <BSorokin@lanit.ru>
2018-04-18 10:36:02 +0400

    Второй автоматизатор создал новый класс

---
+++ branch/src/main/java/secondAutoTesterPackage/NewSecondAutotesters.java
@@ -0,0 +1,10 @@
+package secondAutoTesterPackage;
+
+/**
+ * Created by Sorokin Boris on 18.04.2018.
+ */
+public class NewSecondAutotesters {
+    public static void main(String[] args) {
+        System.out.println("NewSecondAutoTesters class");
+    }
+}
